beaef3e52a1fbb8250b8c705d1b9b664918fdb8b
Abhishek09yadav <monuy8830@gmail.com>
2025-09-17 16:51:08 +0530

    Implement FullScreenLoader component with HashLoader spinner

--- frontend/src/components/loader/FullScreenLoader.jsx
+++ frontend/src/components/loader/FullScreenLoader.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { HashLoader } from 'react-spinners';
 
 const FullScreenLoader = () => {
   return (
-    <div>
-      
+    <div className="fixed inset-0 z-50 grid place-items-center backdrop-blur-sm ">
+      <HashLoader color="#4f46e5" size={80} />
     </div>
-  )
+  );
 }
 
 export default FullScreenLoader
